6e60fc899164fba3b8aa90d4510d2e2a4620e65f
Tesla <mjfan1330@yahoo.com>
2023-10-12 16:01:46 +0200

    Added gitignore and premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "ImGuizmo"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"GraphEditor.h",
+		"GraphEditor.cpp",
+		"ImCurveEdit.h",
+		"ImCurveEdit.cpp",
+		"ImGradient.h",
+		"ImGradient.cpp",
+      "ImGuizmo.h",
+		"ImGuizmo.cpp",
+      "ImSequencer.h",
+		"ImSequencer.cpp",
+		"ImZoomSlider.h"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
